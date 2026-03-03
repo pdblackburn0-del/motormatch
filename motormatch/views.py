c7bf142091555493f0fa98351c676cbfe1e3ce03
Pete Blackburn <p.d.blackburn0@gmail.com>
2026-03-03 15:03:44 +0000

    Launch to heroko

---
+++ motormatch/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something.")
+    return HttpResponse(request.method)
